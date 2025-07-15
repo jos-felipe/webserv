8dc0e1c3f0ef1c36b3d1625af9c8f450edcfdfaa
thiago <thiagosantanadantas@gmail.com>
2025-07-15 14:28:31 -0300

    WIP: Add Logger.hpp

---
+++ include/Logger.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Logger.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asanni <asanni@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/05 14:17:10 by asanni            #+#    #+#             */
+/*   Updated: 2025/07/05 14:17:14 by asanni           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef LOG_LEVEL
+# define LOG_LEVEL
+
+# include <iostream>
+# include <string>
+
+enum LogLevel {
+	CRITICAL,
+	ERROR,
+	WARNING,
+	INFO,
+	DEBUG
+};
+
+class Logger {
+private:
+	LogLevel _level;
+
+public:
+	Logger(void);
+	Logger(const Logger& other);
+	Logger& operator=(const Logger& other);
+	~Logger(void);
+
+	void setLevel(LogLevel level);
+	LogLevel getLevel(void) const;
+
+	void log(LogLevel level, const std::string& message) const;
+	std::ostringstream oss;
+
+	void error(const std::string& message) const;
+	void info(const std::string& message) const;
+	void debug(const std::string& message) const;
+};
+
+# endif
