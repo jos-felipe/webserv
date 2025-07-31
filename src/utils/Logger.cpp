73ff50d35682a8b47907f7fffffa43c76818e711
jos-felipe <139470730+jos-felipe@users.noreply.github.com>
2025-07-31 12:50:29 -0300

    refactor: Logger.cpp / modify setter to receive LogLevel as parameter, agreggate prints inside log() method

--- src/utils/Logger.cpp
+++ src/utils/Logger.cpp
@@ -6,86 +6,48 @@
 /*   By: asanni <asanni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 17:23:07 by asanni            #+#    #+#             */
-/*   Updated: 2025/07/26 17:50:08 by asanni           ###   ########.fr       */
+/*   Updated: 2025/07/31 12:39:16 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Logger.hpp"
-#include <iostream>
-#include <cctype>
 
-Logger::Logger(void) : _level(LOG_LEVEL) {}
+Logger::Logger() : _filterLevel(LOG_LEVEL) {}
 
+Logger::Logger(LogLevel level) : _filterLevel(level) {}
 
-Logger::Logger(const Logger& other) : _level(other._level) {}
+Logger::Logger(const Logger& other) {
+	*this = other;
+}
 
 Logger& Logger::operator=(const Logger& other) {
-	if (this != &other)
-		_level = other._level;
+	if (this != &other) {
+		_filterLevel = other._filterLevel;
+	}
 	return *this;
 }
 
-Logger::~Logger(void) {}
+Logger::~Logger() {}
 
-void Logger::setLevel(std::string level) {
-	level = toLower(level);
-	if (level == "critical")
-		_level = LOG_CRITICAL;
-	else if(level == "error")
-		_level = LOG_ERROR;
-	else if(level == "warning")
-		_level = LOG_WARNING;
-	else if(level == "info")
-		_level = LOG_INFO;
-	else if(level == "debug")
-		_level = LOG_DEBUG;
-	else {
-		std::cerr << "[Logger] Nível inválido: \"" << level
-		          << "\". Usando padrão: INFO." << std::endl;
-		_level = LOG_INFO;
-	}
+void Logger::setFilterLevel(LogLevel level) {
+	_filterLevel = level;
 }
 
-LogLevel Logger::getLevel(void) const {
-	return _level;
-}
-
-void Logger::critical(const std::string& message) const {
-	std::cerr << "[CRITICAL] " << message << std::endl;
-}
-
-void Logger::error(const std::string& message) const {
-	std::cerr << "[ERROR] " << message << std::endl;
-}
-
-void Logger::warning(const std::string& message) const {
-	std::cout << "[WARNING] " << message << std::endl;
-}
-
-void Logger::info(const std::string& message) const {
-	std::cout << "[INFO] " << message << std::endl;
-}
-
-void Logger::debug(const std::string& message) const {
-	std::cout << "[DEBUG] " << message << std::endl;
+LogLevel Logger::getFilterLevel() const {
+	return _filterLevel;
 }
 
 void Logger::log(LogLevel level, const std::string& message) const {
-	//std::cerr << "[LOG DEBUG] _level: " << _level << ", mensagem: " << level << std::endl;
-	if (level < _level)
-		return;
-	switch (level) {
-		case LOG_CRITICAL: critical(message); break;
-		case LOG_ERROR:    error(message);    break;
-		case LOG_WARNING:  warning(message);  break;
-		case LOG_INFO:     info(message);     break;
-		case LOG_DEBUG:    debug(message);    break;
+	if (level >= _filterLevel) {
+		if (level == LOG_DEBUG)
+			std::cout << "[DEBUG] " << message << std::endl;
+		else if (level == LOG_INFO)
+			std::cout << "[INFO] " << message << std::endl;
+		else if (level == LOG_WARNING)
+			std::cout << "[WARNING] " << message << std::endl;
+		else if (level == LOG_ERROR)
+			std::cerr << "[ERROR] " << message << std::endl;
+		else if (level == LOG_CRITICAL)
+			std::cerr << "[CRITICAL] " << message << std::endl;
 	}
-}
-
-std::string Logger::toLower(std::string level) {
-	for (std::string::size_type i = 0; i < level.length(); ++i) {
-		level[i] = std::tolower(level[i]);
-	}
-	return level;
 }
